753c8c7106fa4b2671334899044dc2c03a0969ab
Naveen Kumar Gupta <124282752+nvikumar@users.noreply.github.com>
2026-01-01 22:24:39 +0530

    Refactor byte ordering logic in reverse_the_byte_ordering.v
    Commented out the alternative byte ordering logic.

--- VerilogLanguage/02 Vectors/03_reverse_the_byte_ordering.v
+++ VerilogLanguage/02 Vectors/03_reverse_the_byte_ordering.v
@@ -8,4 +8,16 @@ module top_module(
     
     assign out = {in[7 -: 8],in[8 +: 8] ,in[16 +: 8] , in[24 +: 8]};
 
+
+     // m = 2
+    /* 
+        always @(*)
+        begin 
+                for(int i = 0; i<4; i++)
+        begin
+            out [8*i +: 8] = in[8*(4-i)-1 -: 8]; 
+        end
+        end
+        */
+
 endmodule

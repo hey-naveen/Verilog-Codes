0a679e16ca96f7fbf1eee427f6a733cf9c044798
Naveen Kumar Gupta <124282752+nvikumar@users.noreply.github.com>
2026-01-11 12:47:26 +0530

    Update 08_avoiding_latches.v

--- VerilogLanguage/04 Procedures/08_avoiding_latches.v
+++ VerilogLanguage/04 Procedures/08_avoiding_latches.v
@@ -6,7 +6,6 @@ module top_module (
     output reg up);
     
     always @(*) begin
-
       // assigning default values
         {up, down, left, right} = 4'b0000;
         // left = 0; down = 0; right = 0; up = 0;
@@ -20,4 +19,17 @@ module top_module (
             default : ;  // now default is unnecessary
         endcase
     end
+
+    // m == 2 : common and lengthy way
+    /*
+    always @(*) begin
+        case(scancode)
+            16'he06b: begin left = 1'b1; down = 0; right = 0; up = 0; end
+            16'he072: begin left = 0; down = 1'b1; right = 0; up = 0; end
+            16'he074: begin left = 0; down = 0; right = 1'b1; up = 0; end
+            16'he075: begin left = 0; down = 0; right = 0; up = 1'b1; end
+            default : begin left = 0; down = 0; right = 0; up = 0; end
+        endcase
+    end
+    */
 endmodule

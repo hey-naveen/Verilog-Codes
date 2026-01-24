7e68e6260c61bcaa0087d69b05b39fa806d7f48f
Naveen G. <124282752+nvikumar@users.noreply.github.com>
2026-01-24 10:31:43 +0530

    Enhance comments for output assignment logic
    Added comments to clarify the SOP and POS forms for output assignment.

--- 03_Combinational_Circuits/04_k_mapToCircuits/03_kmap3_4var.v
+++ 03_Combinational_Circuits/04_k_mapToCircuits/03_kmap3_4var.v
@@ -6,6 +6,11 @@ module top_module(
     output out  ); 
 
   // dont care can be 0 or 1, depending upon our use
-  assign out = a | (c & ~b);
+    assign out = a | (c & ~b);     // SOP form
+
+    // grouping zeros and taking 0 as A and 1 as ~A.
+    // assign out = (a | ~b) & (a | c);    // POS form
+
+    
   
 endmodule

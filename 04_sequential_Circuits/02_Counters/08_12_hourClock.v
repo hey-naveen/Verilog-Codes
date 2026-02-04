dd0f6b8bc076d04b52d08c4dd742412c98bd870c
Naveen G. <124282752+nvikumar@users.noreply.github.com>
2026-02-04 21:54:45 +0530

    Update 08_12_hourClock.v

--- 04_sequential_Circuits/02_Counters/08_12_hourClock.v
+++ 04_sequential_Circuits/02_Counters/08_12_hourClock.v
@@ -8,20 +8,23 @@ module top_module(
     output [7:0] ss); 
     
     always @(posedge clk) begin
-        if (reset) begin
+        if (reset) begin                            // 12:00:00 AM
             {hh,mm,ss} <= {8'h12,8'h00,8'h00};		// using hexadecimal to represent bcd
             pm <= 1'b0;
         end
         else if (ena) begin
+            // ---- SECONDS RollOver-----
             if (ss[3:0] == 4'h9) begin
                 ss[3:0] <= 4'h0;
                 if (ss[7:4] == 4'h5) begin
                     ss[7:4] <= 4'h0;
+                    // ---- MINUTES RollOver-----
                     if (mm[3:0] == 4'h9) begin
                         mm[3:0] <= 4'h0;
                         if (mm[7:4] == 4'h5) begin
                             mm[7:4] <= 4'h0;
-                            if (hh == 8'h11) pm <= ~pm;
+                            // ---- HOURS RollOver-----
+                            if (hh == 8'h11) pm <= ~pm;        // PM RollOver 11:59:59 → 12:00:00
                             if ((hh[7:4] == 4'h1) && (hh[3:0]==4'h2)) begin
                                 hh <= 8'h01;
                             end
